e231f96528be81feb514746c4c7b546cc0a38746
camibazan28 <camibazan79@gmail.com>
2024-07-22 20:42:09 -0400

    ciclo while
    tarea ciclo while

---
+++ seccion-4.4/app.js
@@ -0,0 +1,14 @@
+
+
+let gasolina = 30;
+
+while (gasolina > 0) {
+
+  console.log('Gasolina restante' + gasolina);
+  
+  //gasolina=gasolina -1:
+  gasolina --;
+ 
+
+}
+console.log('Ya no tiene gasolina');
