bd4edf241975663c0400eedd82296e24d8f01d9c
camibazan28 <camibazan79@gmail.com>
2024-07-22 20:39:05 -0400

    estructura de control
    tarea de estructura de control

---
+++ seccion-4.1/app.js
@@ -0,0 +1,20 @@
+
+//crear un algoritmo, basado en la nota
+//imprima si el alumno aprueba la materia
+// aprueba 60
+//imprimir un mensaje que diga por favor que estudie mas
+
+
+let nota= 40;
+
+if(nota > 60){
+    console.log('El alumno aprueba la clase');
+}  else if(nota< 50) {
+
+    console.log('Por Favor estudie mas');
+    }  else {
+        console.log('El alumno NO aprueba la clase');
+}
+
+
+console.log('Fin del programa');
