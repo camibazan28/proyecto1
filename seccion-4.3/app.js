e54e192d85ad57fd14430f6a59368dd84053e3a0
camibazan28 <camibazan79@gmail.com>
2024-07-22 20:41:26 -0400

    switch
    tarea switch

---
+++ seccion-4.3/app.js
@@ -0,0 +1,36 @@
+
+
+let dia = 1; // 1= domingo
+
+
+switch ( dia) {
+    case 0:
+        console.log ('Es domingo');
+        break;
+
+     case 1:
+      console.log ('Es lunes');
+       break;
+
+     case 2:
+        console.log ('Es martes');
+        break;
+        
+     case 3:
+      console.log ('Es miercoles');
+       break;
+
+     case 4:
+    console.log ('Es jueves');
+    break;
+
+     case 5:
+     console.log ('Es viernes');
+     break;
+
+     case 6:
+    console.log ('Es Sabado');
+      break;
+      default:
+        console.log('El dia ingresado no es permitido');
+}
